5f39cff2ffbd2b2233e2e5ebd4e978e28f048642
Neil Pretorius <neil@mernok.co.za>
2019-02-26 23:26:24 +0200

    Update ME-PCB-173-03-Ports.c

--- BSP/ME-PCB-173-03-Ports.c
+++ BSP/ME-PCB-173-03-Ports.c
@@ -260,6 +260,16 @@ bool GetButton(void)
 }
 
 
+bool GetTilt(void)
+{
+#ifndef NOTTIliT
+	if(HAL_GPIO_ReadPin(TILT_IN_PORT, TILT_IN_PIN))
+		return false;
+	else
+		return true;
+#endif
+}
+
 //********* MISC IO  functions ***************************************************************************//
 //********************************************************************************************************//
 
